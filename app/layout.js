e555820ce416f629a05f3c2ada9a640cc7d2ac91
iñaki <debau123@gmail.com>
2025-01-21 14:36:10 +0000

    solucionado el nav link

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/app/ui/nav-bar.js ";
+import Navbar from "@/app/ui/nav-bar.js";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
